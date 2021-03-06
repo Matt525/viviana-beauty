f75ba4101125dfa2f77562cfd004cfe5b04aee29
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2021-03-06 00:33:46 -0500

    initial commit & build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Landing from './Landing';
+import About from './About';
+import ReactDOM from 'react-dom'
+import ReactFullpage from '@fullpage/react-fullpage';
+import './CSS/index.css'
+
+const App = () => (
+  <ReactFullpage
+      //options
+      scrollingSpeed={1000}
+      fadingEffect={true}
+      render={() => {
+      return (
+        
+            <ReactFullpage.Wrapper>
+              
+                <section className="section"><Landing id="landing" /></section>
+                
+                
+                <section className="section"><About className="about"/></section>
+                
+            </ReactFullpage.Wrapper>
+        
+        );
+      }}
+      />
+    );
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
